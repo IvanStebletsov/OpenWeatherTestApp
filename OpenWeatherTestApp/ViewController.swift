c9ba643c5ae53e13a5f5e3e3a14fde1f4aa40fa7
Ivan Stebletsov <ivan.stebletsov@gmail.com>
2019-03-05 19:36:51 +0300

    Initial Commit

---
+++ OpenWeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherTestApp
+//
+//  Created by Ivan Stebletsov on 05/03/2019.
+//  Copyright © 2019 Ivan Stebletsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
